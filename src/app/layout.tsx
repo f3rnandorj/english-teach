87685332263880c4727fca28eb16b5b708aec0a4
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2024-05-02 11:44:20 -0300

    Add meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import { pageAssets } from "@/assets";
 import { NavBar } from "@/components";
+import Head from "next/head";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -22,6 +23,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s){
+                if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window,document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '5852912948166338');
+                fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=5852912948166338&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </Head>
       <body
         className={`${poppins.variable}`}
         style={{ backgroundImage: `url(${pageAssets.background.src})` }}
