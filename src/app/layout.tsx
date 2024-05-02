77af84542a577ccd6ae6fb5723934f26ac31bbad
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2024-05-02 14:08:07 -0300

    Add pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,7 @@ export default function RootLayout({
               );
               fbq("init", "5852912948166338");
               fbq("track", "PageView");
+              fbq('track', 'InitiateCheckout');
             `,
         }}
       />
